f80372d82aafcd16fd1481b79fceaf017641fa0a
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2025-01-22 14:22:30 +0700

    Remove unused package

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,6 @@
 import { FC } from "react";
 import { RecoilProvider } from "@/providers/recoil/provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { Toaster } from "@/components/ui/sonner";
 
 const Providers: FC<{ children: any }> = ({ children }) => {
@@ -13,12 +12,6 @@ const Providers: FC<{ children: any }> = ({ children }) => {
         <TooltipProvider delayDuration={50}>{children}</TooltipProvider>
       </RecoilProvider>
       <Toaster />
-      <ProgressBar
-        height="4px"
-        color="#000000"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
     </>
   );
 };
